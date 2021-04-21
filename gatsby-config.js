c9d8ecb0d21e031ec31fedc42445cf3f549740d8
Leonardo Fernandez <leonardo.fernandez1402@gmail.com>
2021-04-21 01:29:04 -0300

    changing siteMetaData

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Leonardo Fernández`,
+    description: `Web Developer - React`,
+    author: `Leonardo Fernández`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
