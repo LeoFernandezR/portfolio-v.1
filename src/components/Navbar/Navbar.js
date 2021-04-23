335e39c175ec727a65f73e2bd2ed24666ba9334c
Leonardo Fernandez <leonardo.fernandez1402@gmail.com>
2021-04-23 13:17:31 -0300

    few styles to the navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,11 @@
 import { Link } from 'gatsby'
 import React from 'react'
 
+import { navbar } from './Navbar.module.scss'
+
 const Navbar = () => {
   return (
-    <nav>
+    <nav className={navbar}>
       <Link to='/'>Home</Link>
       <Link to='/skills'>Skills</Link>
       <Link to='/projects'>Projects</Link>
