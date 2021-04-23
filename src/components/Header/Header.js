2adb92fbed97fffb78d3587046d2ecc800da2676
Leonardo Fernandez <leonardo.fernandez1402@gmail.com>
2021-04-23 00:58:37 -0300

    Changin path to components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
 import Navbar from '../Navbar/Navbar'
 
-const Header = ({ siteTitle }) => (
+const Header = () => (
   <header
     style={{
       background: `rebeccapurple`,
