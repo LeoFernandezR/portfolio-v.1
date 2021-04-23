3db0f2f38f1c23f0ea2c99bff70263c282f4cf1e
Leonardo Fernandez <leonardo.fernandez1402@gmail.com>
2021-04-23 01:03:26 -0300

    deleting images, erasing code i wont need

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,33 +1,11 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'gatsby'
 import Navbar from '../Navbar/Navbar'
 
 const Header = () => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <Navbar />
-    </div>
+  <header>
+    <Navbar />
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,36 +1,18 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import * as React from 'react'
 import PropTypes from 'prop-types'
-import { useStaticQuery, graphql } from 'gatsby'
-
-import Header from './Header/Header'
+import 'typeface-roboto'
 import 'normalize.css'
-import Footer from './Footer/Footer'
+
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
 import { background, container } from './layout.module.scss'
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-          author
-        }
-      }
-    }
-  `)
-
   return (
     <div className={background}>
-      <Header siteTitle={data.site.siteMetadata?.title || 'Title'} />
+      <Header />
       <main className={container}>{children}</main>
-      <Footer siteAuthor={data.site.siteMetadata?.author || 'Author'} />
+      <Footer />
     </div>
   )
 }
