a1070eef11f105625a9e058e0b0903250badc606
Leonardo Fernandez <leonardo.fernandez1402@gmail.com>
2021-04-23 21:05:39 -0300

    Styling the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,15 @@
 import * as React from 'react'
+import { Link } from 'gatsby'
 import Navbar from '../Navbar/Navbar'
+import { header, contact__btn } from './header.module.scss'
 
 const Header = () => (
-  <header>
+  <header className={header}>
+    <h1>Lf</h1>
     <Navbar />
+    <Link className={contact__btn} to='/contact'>
+      Contact
+    </Link>
   </header>
 )
 
