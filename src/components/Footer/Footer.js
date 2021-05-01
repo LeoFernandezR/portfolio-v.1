d81f97426be7fcc8b3e70a6c811184ca9a0781c6
Leonardo Fernandez <leonardo.fernandez1402@gmail.com>
2021-05-01 02:40:04 -0300

    Footer component basic styling, min-height to layout

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,51 @@
-import React from "react"
+import React from 'react'
+import { Icon } from '@iconify/react-with-api'
+
+import { footer, footer__nav } from './footer.module.scss'
+
+const social = [
+  {
+    id: 0,
+    name: 'Github',
+    link: 'https://github.com/leofernandezr',
+    icon: 'simple-icons:github',
+  },
+  {
+    id: 1,
+    name: 'LinkedIn',
+    link: 'https://linkedin.com/in/leonardo-fernandezr/',
+    icon: 'simple-icons:linkedin',
+  },
+  {
+    id: 2,
+    name: 'Email',
+    link: 'leonardo.fernandez1402@gmail.com',
+    icon: 'simple-icons:gmail',
+  },
+]
 
 const Footer = () => {
   return (
-    <footer>
-      © {new Date().getFullYear()}, Built with
-      {` `}
-      <a href="https://www.gatsbyjs.com">Gatsby</a>
+    <footer className={footer}>
+      <div>
+        <p>
+          © All rights are reserved | {new Date().getFullYear()} | Leonardo
+          Fernández
+        </p>
+      </div>
+      <nav className={footer__nav}>
+        {social.map(item => (
+          <a
+            href={item.link}
+            aria-label={item.name}
+            key={item.id}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <Icon icon={item.icon} />
+          </a>
+        ))}
+      </nav>
     </footer>
   )
 }
