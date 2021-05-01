31b7b16dbb50e29a7e53693b717bbb335e4cd8cf
Leonardo Fernandez <leonardo.fernandez1402@gmail.com>
2021-05-01 03:50:30 -0300

    Contact component and basic styling

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
+import { contact, contact__form, contact__image } from './contact.module.scss'
+
+import MailDeliver from '../../images/mail-deliver.svg'
+
 const Contact = () => {
   return (
-    <section>
-      <h1>Contact</h1>
+    <section className={contact}>
+      <form className={contact__form}>
+        <input type='text' placeholder='Name' />
+        <input type='email' placeholder='Email' />
+        <textarea placeholder='Message' cols='30' rows='10'></textarea>
+        <button type='submit'>Submit</button>
+      </form>
+      <div className={contact__image}>
+        <img src={MailDeliver} alt='Send Email Portfolio' />
+      </div>
     </section>
   )
 }
