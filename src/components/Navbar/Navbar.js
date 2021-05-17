ce94ab9813863481831c3d56f2838c5e9b6ae028
Leonardo Fernandez <leonardo.fernandez1402@gmail.com>
2021-05-17 17:50:40 -0300

    Navbar transition

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,27 @@
 import { Link } from 'gatsby'
 import React from 'react'
+import { InlineIcon } from '@iconify/react-with-api'
+import { useLocation } from '@reach/router'
 
-import { navbar } from './Navbar.module.scss'
+import { navbar, active } from './Navbar.module.scss'
 
 const Navbar = () => {
+  const { pathname } = useLocation()
+
   return (
     <nav className={navbar}>
-      <Link to='/'>Home</Link>
-      <Link to='/skills'>Skills</Link>
-      <Link to='/projects'>Projects</Link>
+      <Link to='/' className={pathname === '/' ? active : ''}>
+        <InlineIcon icon='ant-design:home-twotone' />
+        Home
+      </Link>
+      <Link to='/skills' className={pathname === '/skills' ? active : ''}>
+        <InlineIcon icon='ant-design:code-twotone' />
+        Skills
+      </Link>
+      <Link to='/projects' className={pathname === '/projects' ? active : ''}>
+        <InlineIcon icon='ant-design:folder-open-twotone' />
+        Projects
+      </Link>
     </nav>
   )
 }
