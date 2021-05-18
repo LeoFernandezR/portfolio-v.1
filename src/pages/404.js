97bf8800ee66d12c43b7c80616b62ff9f82885b7
Leonardo Fernandez <leonardo.fernandez1402@gmail.com>
2021-05-18 19:57:32 -0300

    Custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,9 +6,23 @@ import Seo from '../components/Seo/Seo'
 const NotFoundPage = () => (
   <Layout>
     <Seo title='404: Not found' />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1 style={notFoundTitle}>404: Not Found</h1>
+    <p style={notFoundParagraph}>
+      You just hit a route that doesn&#39;t exist... the sadness :(
+    </p>
   </Layout>
 )
+
+const notFoundTitle = {
+  color: 'white',
+  fontSize: '3rem',
+  textAlign: 'center',
+}
+
+const notFoundParagraph = {
+  color: 'white',
+  fontSize: '2rem',
+  textAlign: 'center',
+}
 
 export default NotFoundPage
